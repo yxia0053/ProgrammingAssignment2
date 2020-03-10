e347676fa82622f5eed07b62da37ad5840b4a1b9
yxia0053 <17602377780@163.com>
2020-03-10 15:11:41 +1100

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  r <- NULL
+  set <- function(y){
+    x <<- y
+    r <<- NULL
+  }
+  get <- function() x
+  setrevers <- function(slv) r <<- slv
+  getrevers <- function() r
+  list(set = set, get = get,
+       setrevers = setrevers,
+       getrevers = getrevers)
 }
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x,...){
+  r <- x$getrevers()
+  if(!is.null(r)){
+    message("getting cached solve")
+    return(r)
+  }
+  data <- x$get()
+  r <- solve(data,...)
+  x$setrevers(r)
+  r
 }
